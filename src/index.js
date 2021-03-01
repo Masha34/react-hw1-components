92931286c46acb23917a823ad47171006e835052
Maria Savchuk <savchuk_maria@ukr.net>
2021-03-02 01:34:04 +0200

    State+props

--- src/index.js
+++ src/index.js
@@ -16,26 +16,57 @@
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
-import React, {Fragment} from "react";
+import React, {Fragment, Component} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+// import App from './App';
 
-import Header from "./Components/Header/Header";
-import Search from "./Components/Search/Search";
-import ContactList from "./Components/ContactList/ContactList";
-import ContactItem from "./Components/ContactList/ContactItem.js/ContactItem";
-import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/header";
+import Search from "./Components/Search/search";
+import ContactList from "./Components/ContactList/contactList";
+import ContactItem from "./Components/ContactList/ContactItem.js/contactItem";
+import Footer from "./Components/Footer/footer";
 
-const App = () => {
-  return(
-    <Fragment>
-        <Header />
-        <Search />
-        <ContactList />
-        <ContactItem />
-        <ContactItem />
-        <Footer />
-    </Fragment>
-  )
+class App extends Component {
+
+  state = {
+    List: [
+      {
+        "Avatar": "https://www.bootdey.com/img/Content/avatar/avatar3.png",
+        "Name": "Mila Kunis",
+        "Created": "2013/08/08",
+        "Role": "Admin",
+        "Status": "Active",
+        "Email": "mila@kunis.com"
+      },
+      {
+        "Avatar": "https://www.bootdey.com/img/Content/avatar/avatar1.png",
+        "Name": "Camil Jonson",
+        "Created": "2013/08/08",
+        "Role": "User",
+        "Status": "Inactive",
+        "Email": "camil@gmail.com"
+      },
+      {
+        "Avatar": "https://www.bootdey.com/img/Content/avatar/avatar2.png",
+        "Name": "Mike Daniels",
+        "Created": "2013/08/08",
+        "Role": "User",
+        "Status": "Active",
+        "Email": "mike@ukr.com"
+      }
+    ]
+  }
+  render(){
+    const {List} = this.state;
+    return(
+      <Fragment>
+          <Header />
+          <Search />
+          <ContactList List={List} />
+          <Footer />
+      </Fragment>
+    )
+  }
 }
 ReactDOM.render(<App />, document.getElementById("root"));
